d1cf8a29147aacdc4d34a3c5f05344b3158367ea
fabricemoeneclaey <67113051+fabricemoeneclaey@users.noreply.github.com>
2020-06-19 09:12:37 +0200

    Update create_table.py

--- create_table.py
+++ create_table.py
@@ -15,3 +15,36 @@ def init_db(continent):
             print(country)
             info = json.loads(z.read(f))
             save_country(conn, country, info)
+
+def get_name(wp_info):
+    if 'conventional_long_name' in wp_info:
+        name = wp_info['conventional_long_name']
+        m = re.match("([\w, -]+?)\s*{{", name)
+        if m:
+            name = m.group(1)
+        m = re.match("{{.*\|([\w, -]+)}}", name)
+        if m:
+            name = m.group(1)
+        return name
+    if 'common_name' in wp_info and wp_info['common_name'] == 'Singapore':
+        return 'Republic of Singapore'
+    if 'common_name' in wp_info:
+        name = wp_info['common_name']
+        print('using common name {}...'.format(name), end='')
+        return name
+    print('Could not fetch country name {}'.format(wp_info))
+    return None
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
